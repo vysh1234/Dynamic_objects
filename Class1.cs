620f6dbf82fced0dc0846d344fbc2569cbe7f576
vysh1234 <75359385+vysh1234@users.noreply.github.com>
2021-10-20 12:57:18 +0530

    c#

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dynamic_object
+{
+    class Dynamic_Object
+    {
+        public Dynamic_Object() { }
+        public Dynamic_Object(String Msg) { }
+        public void First_Method() {
+            Console.WriteLine("inside first method");
+        }
+        public void Second_Method() {
+            Console.WriteLine("inside second method");
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dynamic_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Dynamic_Object obj1 = new Dynamic_Object();
+            obj1.First_Method();
+            obj1.Second_Method();
+
+            dynamic obj2 = new Dynamic_Object();
+            obj2.First_Method();
+            obj2.Second_Method();
+        }
+    }
+}
